04c07d147a6257819be9579a84ee3f5b62a00c40
chrislin123 <chris.lin.tw@hotmail.com>
2016-03-30 23:43:49 +0800

    1050330

---
+++ M10CalRtiDetail/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace M10CalRtiDetail
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ddlver = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnstart = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ddlver
+            // 
+            this.ddlver.FormattingEnabled = true;
+            this.ddlver.Location = new System.Drawing.Point(93, 41);
+            this.ddlver.Name = "ddlver";
+            this.ddlver.Size = new System.Drawing.Size(121, 20);
+            this.ddlver.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 102);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1334, 457);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnstart
+            // 
+            this.btnstart.Location = new System.Drawing.Point(456, 39);
+            this.btnstart.Name = "btnstart";
+            this.btnstart.Size = new System.Drawing.Size(75, 23);
+            this.btnstart.TabIndex = 2;
+            this.btnstart.Text = "開始轉檔";
+            this.btnstart.UseVisualStyleBackColor = true;
+            this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3"});
+            this.comboBox1.Location = new System.Drawing.Point(237, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1358, 571);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnstart);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ddlver);
+            this.Name = "MainForm";
+            this.Text = "M10_Cal_RtiDetail";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox ddlver;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnstart;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ M10CalRtiDetail/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CL.Data;
+using System.Data.Common;
+using System.Globalization;
+
+
+namespace M10CalRtiDetail
+{
+    public partial class MainForm : Form
+    {
+        string ssql = string.Empty;
+        string sConnectionString = Properties.Settings.Default.DBConnectionString;
+        ODAL oDal = new ODAL(Properties.Settings.Default.DBConnectionString);
+        DataTable dt_rtidetail = new DataTable();
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //建立資料表基本資料
+                buildtable();
+
+
+                dataGridView1.DataSource = dt_rtidetail;
+                dataGridView1.AutoGenerateColumns = true;
+
+
+
+                DataTable dt = new DataTable();
+                DataTable dt1 = new DataTable();
+
+                //取得所有版本號
+                ssql = " select distinct ver from RtiData ";
+                oDal.CommandText = ssql;
+                dt.Clear();
+                dt = oDal.DataTable();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    ddlver.Items.Add(dr["ver"].ToString());
+                }
+
+
+                //取得所有站號
+                ssql = " select distinct station from RtiData"
+                             + "  order by station  "
+                             ;
+                oDal.CommandText = ssql;
+                dt.Clear();
+                dt = oDal.DataTable();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    DataRow newdr = dt_rtidetail.NewRow();
+
+                    newdr["station"] = dr["station"].ToString();
+                    dt_rtidetail.Rows.Add(newdr);
+                }
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+        }
+
+
+
+
+        private void buildtable()
+        {
+            dt_rtidetail.Columns.Add("station");
+            dt_rtidetail.Columns.Add("delaytime");
+            dt_rtidetail.Columns.Add("totalcount");
+            dt_rtidetail.Columns.Add("startdate");
+            dt_rtidetail.Columns.Add("enddate");
+            dt_rtidetail.Columns.Add("rti10");
+            dt_rtidetail.Columns.Add("rti30");
+            dt_rtidetail.Columns.Add("rti50");
+            dt_rtidetail.Columns.Add("rti70");
+            dt_rtidetail.Columns.Add("rti90");
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int rowindex = 0;
+                foreach (DataRow dr in dt_rtidetail.Rows)
+                {
+                    string sStation = dr["station"].ToString();
+
+                    //移動資料行
+                    dataGridView1.CurrentCell = dataGridView1.Rows[rowindex].Cells[0];
+                    
+                    //取得總筆數                
+                    ssql = " select count(*) as total from RtiData "
+                         + " where station = '" + sStation + "' ";
+                    oDal.CommandText = ssql;
+                    string sTotalcount = oDal.Value().ToString().Trim();
+                    dr["totalcount"] = sTotalcount;
+
+                    Application.DoEvents();
+
+                    //取得開始時間
+                    ssql = " select MIN(date)  from RtiData "
+                         + " where station = '" + sStation + "' ";
+                    oDal.CommandText = ssql;
+                    string sStartDate = oDal.Value().ToString().Trim();
+                    dr["startdate"] = sStartDate;
+
+                    Application.DoEvents();
+
+
+                    //取得結束時間
+                    ssql = " select MAX(date)  from RtiData "
+                         + " where station = '" + sStation + "' ";
+                    oDal.CommandText = ssql;
+                    string sEndDate = oDal.Value().ToString().Trim();
+                    dr["enddate"] = sEndDate;
+
+                    Application.DoEvents();
+
+
+
+                    rowindex++;
+                }
+
+                
+
+
+
+                
+                
+
+
+
+
+                
+
+
+
+
+            }
+            catch (Exception)
+            {   
+                throw;
+            }
+        }
+    }
+}
