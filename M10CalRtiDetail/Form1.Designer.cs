0f73fa0252d253edf8c0d6c7ea37f1606ea9fc7f
chrislin123 <chris.lin.tw@hotmail.com>
2016-04-12 00:28:43 +0800

    1050412

--- M10CalRtiDetail/Form1.Designer.cs
+++ M10CalRtiDetail/Form1.Designer.cs
@@ -31,64 +31,198 @@
             this.ddlver = new System.Windows.Forms.ComboBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.btnstart = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ddlTimeDelay = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRTI10 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.lblrti10 = new System.Windows.Forms.Label();
+            this.lblrti30 = new System.Windows.Forms.Label();
+            this.lblrti50 = new System.Windows.Forms.Label();
+            this.lblrti70 = new System.Windows.Forms.Label();
+            this.lblrti90 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // ddlver
             // 
+            this.ddlver.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.ddlver.FormattingEnabled = true;
-            this.ddlver.Location = new System.Drawing.Point(93, 41);
+            this.ddlver.Location = new System.Drawing.Point(216, 20);
+            this.ddlver.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.ddlver.Name = "ddlver";
-            this.ddlver.Size = new System.Drawing.Size(121, 20);
+            this.ddlver.Size = new System.Drawing.Size(199, 28);
             this.ddlver.TabIndex = 0;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 102);
+            this.dataGridView1.Location = new System.Drawing.Point(14, 101);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1334, 457);
+            this.dataGridView1.Size = new System.Drawing.Size(1417, 666);
             this.dataGridView1.TabIndex = 1;
             // 
             // btnstart
             // 
-            this.btnstart.Location = new System.Drawing.Point(456, 39);
+            this.btnstart.Location = new System.Drawing.Point(489, 53);
+            this.btnstart.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnstart.Name = "btnstart";
-            this.btnstart.Size = new System.Drawing.Size(75, 23);
+            this.btnstart.Size = new System.Drawing.Size(125, 38);
             this.btnstart.TabIndex = 2;
             this.btnstart.Text = "開始轉檔";
             this.btnstart.UseVisualStyleBackColor = true;
             this.btnstart.Click += new System.EventHandler(this.btnstart_Click);
             // 
-            // comboBox1
+            // ddlTimeDelay
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.ddlTimeDelay.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddlTimeDelay.FormattingEnabled = true;
+            this.ddlTimeDelay.Items.AddRange(new object[] {
             "0",
             "1",
             "2",
             "3"});
-            this.comboBox1.Location = new System.Drawing.Point(237, 42);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 20);
-            this.comboBox1.TabIndex = 3;
+            this.ddlTimeDelay.Location = new System.Drawing.Point(216, 63);
+            this.ddlTimeDelay.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.ddlTimeDelay.Name = "ddlTimeDelay";
+            this.ddlTimeDelay.Size = new System.Drawing.Size(199, 28);
+            this.ddlTimeDelay.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "工作延時：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // btnRTI10
+            // 
+            this.btnRTI10.Location = new System.Drawing.Point(642, 53);
+            this.btnRTI10.Margin = new System.Windows.Forms.Padding(5);
+            this.btnRTI10.Name = "btnRTI10";
+            this.btnRTI10.Size = new System.Drawing.Size(125, 38);
+            this.btnRTI10.TabIndex = 6;
+            this.btnRTI10.Text = "轉檔RTI10";
+            this.btnRTI10.UseVisualStyleBackColor = true;
+            this.btnRTI10.Click += new System.EventHandler(this.btnRTI10_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(42, 123);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(5);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(572, 534);
+            this.dataGridView2.TabIndex = 7;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(701, 123);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(5);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(572, 534);
+            this.dataGridView3.TabIndex = 8;
+            // 
+            // lblrti10
+            // 
+            this.lblrti10.AutoSize = true;
+            this.lblrti10.Location = new System.Drawing.Point(786, 66);
+            this.lblrti10.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblrti10.Name = "lblrti10";
+            this.lblrti10.Size = new System.Drawing.Size(89, 20);
+            this.lblrti10.TabIndex = 9;
+            this.lblrti10.Text = "工作延時：";
+            // 
+            // lblrti30
+            // 
+            this.lblrti30.AutoSize = true;
+            this.lblrti30.Location = new System.Drawing.Point(904, 66);
+            this.lblrti30.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblrti30.Name = "lblrti30";
+            this.lblrti30.Size = new System.Drawing.Size(89, 20);
+            this.lblrti30.TabIndex = 10;
+            this.lblrti30.Text = "工作延時：";
+            // 
+            // lblrti50
+            // 
+            this.lblrti50.AutoSize = true;
+            this.lblrti50.Location = new System.Drawing.Point(1022, 66);
+            this.lblrti50.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblrti50.Name = "lblrti50";
+            this.lblrti50.Size = new System.Drawing.Size(89, 20);
+            this.lblrti50.TabIndex = 11;
+            this.lblrti50.Text = "工作延時：";
+            // 
+            // lblrti70
+            // 
+            this.lblrti70.AutoSize = true;
+            this.lblrti70.Location = new System.Drawing.Point(1140, 66);
+            this.lblrti70.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblrti70.Name = "lblrti70";
+            this.lblrti70.Size = new System.Drawing.Size(89, 20);
+            this.lblrti70.TabIndex = 12;
+            this.lblrti70.Text = "工作延時：";
+            // 
+            // lblrti90
+            // 
+            this.lblrti90.AutoSize = true;
+            this.lblrti90.Location = new System.Drawing.Point(1258, 66);
+            this.lblrti90.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblrti90.Name = "lblrti90";
+            this.lblrti90.Size = new System.Drawing.Size(89, 20);
+            this.lblrti90.TabIndex = 13;
+            this.lblrti90.Text = "工作延時：";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1358, 571);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(1445, 781);
+            this.Controls.Add(this.lblrti90);
+            this.Controls.Add(this.lblrti70);
+            this.Controls.Add(this.lblrti50);
+            this.Controls.Add(this.lblrti30);
+            this.Controls.Add(this.lblrti10);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.btnRTI10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ddlTimeDelay);
             this.Controls.Add(this.btnstart);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.ddlver);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "MainForm";
             this.Text = "M10_Cal_RtiDetail";
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -97,7 +231,17 @@
         private System.Windows.Forms.ComboBox ddlver;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button btnstart;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox ddlTimeDelay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRTI10;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label lblrti10;
+        private System.Windows.Forms.Label lblrti30;
+        private System.Windows.Forms.Label lblrti50;
+        private System.Windows.Forms.Label lblrti70;
+        private System.Windows.Forms.Label lblrti90;
     }
 }
 
--- M10CalRtiDetail/Form1.cs
+++ M10CalRtiDetail/Form1.cs
@@ -20,6 +20,8 @@ namespace M10CalRtiDetail
         string sConnectionString = Properties.Settings.Default.DBConnectionString;
         ODAL oDal = new ODAL(Properties.Settings.Default.DBConnectionString);
         DataTable dt_rtidetail = new DataTable();
+        DataTable dt_rti = new DataTable();
+        DataTable dt_rti90 = new DataTable();
 
 
         public MainForm()
@@ -35,9 +37,19 @@ namespace M10CalRtiDetail
                 //建立資料表基本資料
                 buildtable();
 
+                //建立資料表基本資料
+                buildtableRTI();
+                
+
 
                 dataGridView1.DataSource = dt_rtidetail;
                 dataGridView1.AutoGenerateColumns = true;
+
+                dataGridView2.DataSource = dt_rti;
+                dataGridView2.AutoGenerateColumns = true;
+
+                dataGridView3.DataSource = dt_rti90;
+                dataGridView3.AutoGenerateColumns = true;
 
 
 
@@ -53,6 +65,9 @@ namespace M10CalRtiDetail
                 {
                     ddlver.Items.Add(dr["ver"].ToString());
                 }
+
+                ddlTimeDelay.SelectedIndex = 0;
+                
 
 
                 //取得所有站號
@@ -75,6 +90,32 @@ namespace M10CalRtiDetail
                 
                 throw;
             }
+        }
+
+        private void buildtableRTI()
+        {
+            dt_rti.Columns.Add("index");
+            dt_rti.Columns.Add("station");
+            dt_rti.Columns.Add("rti");
+            dt_rti.Columns.Add("totalcount");
+            dt_rti.Columns.Add("pas");
+
+
+            dt_rti90.Columns.Add("index");
+            dt_rti90.Columns.Add("station");
+            dt_rti90.Columns.Add("rti");
+            dt_rti90.Columns.Add("totalcount");
+            dt_rti90.Columns.Add("pas");
+
+
+            //dt_rti.Columns.Add("totalcount");
+            //dt_rti.Columns.Add("startdate");
+            //dt_rti.Columns.Add("enddate");
+            //dt_rti.Columns.Add("rti10");
+            //dt_rti.Columns.Add("rti30");
+            //dt_rti.Columns.Add("rti50");
+            //dt_rti.Columns.Add("rti70");
+            //dt_rti.Columns.Add("rti90");
         }
 
 
@@ -102,6 +143,10 @@ namespace M10CalRtiDetail
                 foreach (DataRow dr in dt_rtidetail.Rows)
                 {
                     string sStation = dr["station"].ToString();
+
+                    //工作延時(delaytime)
+                    dr["totalcount"] = ddlTimeDelay.Text;                    
+                    Application.DoEvents();
 
                     //移動資料行
                     dataGridView1.CurrentCell = dataGridView1.Rows[rowindex].Cells[0];
@@ -134,31 +179,185 @@ namespace M10CalRtiDetail
 
                     Application.DoEvents();
 
+                    //計算RTI10
+
+
+                    //計算RTI90
+
+
+                    //計算RTI30
+
+
+                    //計算RTI50
+
+
+                    //計算RTI70
+
 
 
                     rowindex++;
                 }
-
-                
-
-
-
-                
-                
-
-
-
-
-                
-
-
-
 
             }
             catch (Exception)
             {   
                 throw;
             }
+        }
+
+        private void btnRTI10_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+
+
+            //取得總筆數                
+            ssql = " select count(*) as total from RtiData "
+                 + " where station = 'C0A510' ";
+            oDal.CommandText = ssql;
+            string sTotalcount = oDal.Value().ToString().Trim();            
+
+            Application.DoEvents();
+
+
+            //取得所有站號
+            ssql = " select * from RtiData"
+                 + "  where station = 'C0A510'  "
+                 + "  order by rti  ";
+
+            oDal.CommandText = ssql;
+            dt.Clear();
+            dt = oDal.DataTable();
+            int iIndex = 1;
+            dt_rti.Clear();
+            foreach (DataRow dr in dt.Rows)
+            {
+                DataRow newdr = dt_rti.NewRow();
+
+                newdr["index"] = iIndex.ToString();
+                newdr["station"] = dr["station"].ToString();
+                newdr["rti"] = dr["rti"].ToString();
+                newdr["totalcount"] = sTotalcount;
+                dt_rti.Rows.Add(newdr);
+
+
+                
+                double dtotalcount = double.Parse(sTotalcount);
+                double dIndex = double.Parse(iIndex.ToString());
+
+                //前三個小時平均 * RT值
+                double dResult = dIndex / (dtotalcount + 1) * 100;
+                string sResult = Math.Round(dResult, 2).ToString();
+
+                newdr["pas"] = sResult;
+
+                iIndex++;
+            }
+
+
+            //找出最接近10%的數值
+            double dRTIA = 0;
+            double dRTIB = 0;
+            double dPASa = 0;
+            double dPASb = 0;
+            double dRTI10 = 0;
+            double dRTI30 = 0;
+            double dRTI50 = 0;
+            double dRTI70 = 0;
+            double dRTI90 = 0;
+          
+            bool bCopy = false;
+            foreach (DataRow dr in dt_rti.Rows)
+            {
+                //暫存前一筆
+                dPASa = dPASb;
+                dRTIA = dRTIB;
+
+                //取得目前這一筆
+                dPASb = double.Parse(dr["pas"].ToString());
+                dRTIB = double.Parse(dr["rti"].ToString());
+
+                //判斷兩組資料是否為10%中間
+                if (dPASa < 10 && dPASb > 10)
+                {
+                    bCopy = true;
+                    //進行計算RTI10
+
+
+                    dRTI10 = ((dRTIB - dRTIA) / (dPASb - dPASa) * (10 - dPASa)) + dRTIA;
+
+                    dRTI10 = Math.Round(dRTI10, 2);                    
+                }
+
+
+                //複製除了rti10之前的資料到dt_rti90
+                if (bCopy)
+                {
+                    DataRow drn = dt_rti90.NewRow();
+                    drn["index"] = dr["index"];
+                    drn["station"] = dr["station"];
+                    drn["rti"] = dr["rti"];
+                    drn["totalcount"] = dr["totalcount"];
+                    drn["pas"] = dr["pas"];
+
+                    dt_rti90.Rows.Add(drn);
+                }
+            }
+
+            //dt_rti90重整
+            int irti90 = 1;
+            foreach (DataRow dr in dt_rti90.Rows)
+            {
+                double dtotalcount = double.Parse(dt_rti90.Rows.Count.ToString());
+                double dIndex = double.Parse(irti90.ToString());
+
+                //前三個小時平均 * RT值
+                double dResult = dIndex / (dtotalcount + 1) * 100;
+                string sResult = Math.Round(dResult, 2).ToString();
+
+                dr["index"] = irti90.ToString();
+                dr["pas"] = sResult;
+
+                irti90++;
+            }
+
+            dRTIA = 0;
+            dRTIB = 0;
+            dPASa = 0;
+            dPASb = 0;
+
+            foreach (DataRow dr in dt_rti90.Rows)
+            {
+                //暫存前一筆
+                dPASa = dPASb;
+                dRTIA = dRTIB;
+
+                //取得目前這一筆
+                dPASb = double.Parse(dr["pas"].ToString());
+                dRTIB = double.Parse(dr["rti"].ToString());
+
+                //判斷兩組資料是否為10%中間
+                if (dPASa < 90 && dPASb > 90)
+                {
+                    //進行計算RTI90
+                    dRTI90 = ((dRTIB - dRTIA) / (dPASb - dPASa) * (90 - dPASa)) + dRTIA;
+
+                    dRTI90 = Math.Round(dRTI90, 2);
+                }
+
+            }
+
+
+            dRTI30 = ((dRTI90 - dRTI10) / 8 * 2) + dRTI10;
+            dRTI50 = ((dRTI90 - dRTI10) / 8 * 4) + dRTI10;
+            dRTI70 = ((dRTI90 - dRTI10) / 8 * 6) + dRTI10;
+
+            
+            lblrti10.Text = dRTI10.ToString();
+            lblrti30.Text = dRTI30.ToString();
+            lblrti50.Text = dRTI50.ToString();
+            lblrti70.Text = dRTI70.ToString();
+            lblrti90.Text = dRTI90.ToString();
         }
     }
 }
