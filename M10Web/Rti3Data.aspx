e2c71c54e5070052d25d1df467af5cdfcfbf9b8a
chrislin123 <chris.lin.tw@hotmail.com>
2016-04-16 22:33:08 +0800

    1050416_1

--- M10Web/Rti3Data.aspx
+++ M10Web/Rti3Data.aspx
@@ -6,12 +6,125 @@
 <head runat="server">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
+    <link rel="stylesheet" type="text/css" href="css/css/bootstrap.css">
+    <link href="css/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/themes/default/easyui.css">
+    <link rel="stylesheet" type="text/css" href="css/themes/icon.css">
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easyui.min.js"></script>
+
+    <style>
+        th.sortasc a {
+            display: block;
+            padding: 0 4px 0 15px;
+            background: url(images/arrow-asc.png) no-repeat;
+        }
+
+        th.sortdesc a {
+            display: block;
+            padding: 0 4px 0 15px;
+            background: url(images/sort-asc-alt.png) no-repeat;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
-    
-    </div>
+     <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <div class="container">
+            <h1>崩塌警戒基準值(RTI3)</h1>
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <%--<h1 class="panel-title"></h1>--%>
+                    <table style="width: 100%;">
+                                <tr>
+                                    <td></td>
+                                    <td style="width: 20%; text-align: right;">
+                                        
+                                        <asp:Button ID="btnBack" runat="server" Text="返回雨量查詢" CssClass="btn btn-success" OnClick="btnBack_Click" /></td>
+                                </tr>
+                            </table>
+                </div>
+                <div class="panel-body">
+                    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                        <ContentTemplate>
+
+                            <div class="form-inline">
+                                <%--<asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>--%>
+
+                                <table>
+                                    <tr>
+                                        <td>
+                                            <label class="label label-success" style="font-size: 20px">降雨延時(F)</label></td>
+                                        <td>
+                                            <asp:RadioButtonList ID="rDelaytime" runat="server" RepeatDirection="Horizontal" AutoPostBack="True" OnSelectedIndexChanged="rDelaytime_SelectedIndexChanged" >
+                                                <asp:ListItem>0</asp:ListItem>
+                                                <asp:ListItem>1</asp:ListItem>
+                                                <asp:ListItem>2</asp:ListItem>
+                                                <asp:ListItem>3</asp:ListItem>
+                                            </asp:RadioButtonList></td>
+                                        <td>
+                                            <%--<asp:Label ID="lblDataTime" runat="server" Text="資料時間:"></asp:Label>--%></td>
+                                    </tr>
+                                </table>
+
+
+
+                                <%--<asp:DropDownList ID="ddlCOUNTY" runat="server" CssClass="form-control" AutoPostBack="true" OnSelectedIndexChanged="ddlCOUNTY_SelectedIndexChanged"></asp:DropDownList>--%>
+                                <div style="display: none;">
+                                    <%--<asp:Button ID="btnQuery" runat="server" Text="查詢" CssClass="btn btn-success" OnClick="btnQuery_Click"  />--%>
+                                </div>
+
+                             <%--   <div style="float: right; font-size: 20px">
+                                </div>--%>
+
+
+
+                            </div>
+                            <%--footable--%>
+                            <%--table table-hover table-striped--%>
+                            <%--CssClass="table table-striped table-bordered table-condensed"--%>
+
+                            <%----%>
+                            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False"
+                                AllowSorting="True" CssClass="table table-striped table-bordered table-condensed"
+                                OnRowCreated="GridView1_RowCreated" OnSorting="GridView1_Sorting" OnRowDataBound="GridView1_RowDataBound">
+                                <Columns>
+                                    <asp:BoundField DataField="station" HeaderText="站號" SortExpression="station" />
+                                    <asp:BoundField DataField="totalcount" HeaderText="筆數" SortExpression="totalcount" />
+                                    <asp:BoundField DataField="startdate" HeaderText="開始時間" SortExpression="startdate" />
+                                    <asp:BoundField DataField="enddate" HeaderText="最終時間" SortExpression="enddate" />
+                                    <asp:BoundField DataField="rti10" HeaderText="RTI10" SortExpression="rti10" />
+                                    <asp:BoundField DataField="rti30" HeaderText="RTI30" SortExpression="rti30" />
+                                    <asp:BoundField DataField="rti50" HeaderText="RTI50" SortExpression="rti50" />
+                                    <asp:BoundField DataField="rti70" HeaderText="RTI70" SortExpression="rti70" />
+                                    <asp:BoundField DataField="rti90" HeaderText="RTI90" SortExpression="rti90" />                                    
+                                </Columns>
+                                <SortedAscendingHeaderStyle CssClass="sortasc" />
+                                <SortedDescendingHeaderStyle CssClass="sortdesc" />
+                            </asp:GridView>
+                            <%--<asp:Timer ID="Timer1" runat="server" OnTick="Timer1_Tick" Interval="1000"></asp:Timer>--%>
+                        </ContentTemplate>
+
+                    </asp:UpdatePanel>
+                </div>
+            </div>
+            <div class="page-header"></div>
+        </div>
+
+        <script type="text/javascript">
+
+            function myrefresh() {
+                //window.location.reload();
+                $("#btnQuery").click();
+            }
+
+            $(function () {
+                setInterval(myrefresh, 60000); //指定10秒刷新一次
+            });
+
+        </script>
     </form>
 </body>
 </html>
--- M10Web/Rti3Data.aspx.cs
+++ M10Web/Rti3Data.aspx.cs
@@ -4,14 +4,206 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CL.Data;
+using System.Data;
 
 namespace M10Web
 {
     public partial class Rti3Data : System.Web.UI.Page
     {
+        ODAL oDal = new ODAL("DBConnectionString");
+        string ssql = string.Empty;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack == false)
+            {
+                //排序預設
+                ViewState["sortExpression"] = "station";
+                ViewState["sort"] = " DESC"; //or DESC              
 
+                //預設選取第一個
+                rDelaytime.SelectedIndex = 0;
+                rDelaytime_SelectedIndexChanged(sender, e);
+            }
+        }
+
+        protected void GridView1_Sorting(object sender, GridViewSortEventArgs e)
+        {
+
+            if (ViewState["SortExpression"] == null)
+            {
+                ViewState["sortExpression"] = e.SortExpression;
+
+                if (ViewState["sort"].ToString() == " DESC")
+                {
+                    ViewState["sort"] = " ASC";
+                    BindNodeInfo();
+                }
+                else
+                {
+                    ViewState["sort"] = " DESC";
+                    BindNodeInfo();
+                }
+            }
+        }
+
+        public void BindNodeInfo()
+        {
+            //ssql = " SELECT * FROM RunTimeRainData where COUNTY = '" + ddlCOUNTY.SelectedValue + "' ";
+
+            //取得更新最新時間
+            //ssql = " select MAX(RTime) as RTime from RunTimeRainData ";
+            //oDal.CommandText = ssql;
+            //DataTable dttime = oDal.DataTable();
+
+            //if (dttime.Rows.Count > 0)
+            //{
+            //    lblDataTime.Text = "資料時間:" + dttime.Rows[0]["RTime"].ToString();
+            //}
+
+
+            //預先轉型態，Gridview可直接排序   
+            //            ssql = @"   select CONVERT(float, MIN10) as MIN10
+            //                        ,CONVERT(float, RAIN) as RAIN
+            //                        ,CONVERT(float, HOUR3) as HOUR3
+            //                        ,CONVERT(float, HOUR6) as HOUR6
+            //                        ,CONVERT(float, HOUR12) as HOUR12
+            //                        ,CONVERT(float, HOUR24) as HOUR24
+            //                        ,CONVERT(float, NOW) as NOW
+            //                        ,ROUND(CONVERT(float, RT),2) as RT
+            //                        ,CONVERT(float, LRTI) as LRTI
+            //                        ,ROUND(CONVERT(float, b.ELRTI),2) as ELRTI
+            //                        ,* from RunTimeRainData a
+            //                        left join StationErrLRTI b on a.STID = b.STID 
+            //                    ";
+            ssql = @"   select * from Rti3Detail
+                        where delaytime = 
+                            
+                        ";
+
+            ssql = " select * from Rti3Detail where delaytime = '{0}' ";
+            ssql = string.Format(ssql, rDelaytime.SelectedValue);
+            ssql += " order by station ";
+
+
+            oDal.CommandText = ssql;
+            DataTable dt = oDal.DataTable();
+
+
+
+
+            //1040806 處理資料異常，所有數值改為0
+            //foreach (DataRow dr in dt.Rows)
+            //{
+            //    if (dr["STATUS"].ToString() == "-99")
+            //    {
+            //        dr["MIN10"] = 0;
+            //        dr["RAIN"] = 0;
+            //        dr["HOUR3"] = 0;
+            //        dr["HOUR6"] = 0;
+            //        dr["HOUR12"] = 0;
+            //        dr["HOUR24"] = 0;
+            //        dr["NOW"] = 0;
+            //        dr["RT"] = 0;
+            //        dr["LRTI"] = 0;
+
+            //    }
+            //}
+
+            DataView myview = dt.DefaultView;
+            myview.Sort = ViewState["sortExpression"].ToString() + " " + ViewState["sort"].ToString();
+            GridView1.DataSource = myview;
+            //NodeGridView.DataKeyNames = new string[] { "node_id" };               //设置主键字段
+            GridView1.DataBind();                                                  //绑定GridView控件  
+        }
+
+        protected void GridView1_RowCreated(object sender, GridViewRowEventArgs e)
+        {
+            //隱藏欄位
+            if (e.Row.RowType == DataControlRowType.DataRow || e.Row.RowType == DataControlRowType.Header)
+            {
+                //e.Row.Cells[15].Visible = false;
+            }
+
+            if (e.Row.RowType == DataControlRowType.Header)
+            {
+                foreach (TableCell tc in e.Row.Cells)
+                {
+                    if (tc.HasControls() == true)
+                    {
+                        if (((LinkButton)tc.Controls[0]).Text == "Duration")
+                        {
+                            ((LinkButton)tc.Controls[0]).Text = GetLocalResourceObject("Duration").ToString();
+                        }
+                        else if (((LinkButton)tc.Controls[0]).Text == "RecordingName")
+                        {
+                            ((LinkButton)tc.Controls[0]).Text = GetLocalResourceObject("RecordingName").ToString();
+                        }
+                        else if (((LinkButton)tc.Controls[0]).Text == "FileSize")
+                        {
+                            ((LinkButton)tc.Controls[0]).Text = GetLocalResourceObject("FileSize").ToString();
+                        }
+
+                        if (((LinkButton)tc.Controls[0]).CommandArgument == ViewState["sort"].ToString())
+                        {
+                            if (ViewState["mySorting"].ToString() == "ASC")
+                            {
+                                tc.Controls.Add(new LiteralControl("↓"));
+                            }
+                            else
+                            {
+                                tc.Controls.Add(new LiteralControl("↑"));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                //if (e.Row.Cells[15].Text == "-99")
+                //{
+                //    e.Row.Cells[15].Text = "異常";              
+
+                //    e.Row.ForeColor = System.Drawing.Color.LightGray;
+                //}
+                //else
+                //{
+                //    e.Row.Cells[15].Text = "";
+                //}
+
+                //警戒值超過要變色
+                //if (e.Row.Cells[13].Text != "" && e.Row.Cells[13].Text != "&nbsp;")
+                //{
+                //    double dLRTI = 0;
+                //    double dELRTI = 0;
+                //    double.TryParse(e.Row.Cells[12].Text, out dLRTI);
+                //    double.TryParse(e.Row.Cells[13].Text, out dELRTI);
+
+                //    if (dLRTI > dELRTI)
+                //    {
+                //        e.Row.ForeColor = System.Drawing.Color.Red;
+                //    }
+                //}
+            }
+
+
+
+        }
+
+        protected void rDelaytime_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BindNodeInfo();
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("default.aspx", true);
         }
     }
 }
