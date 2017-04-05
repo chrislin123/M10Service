40fcf84b2450c86fbc1c76f975e7610357c8a68c
chrislin <chris.lin.tw@hotmail.com>
2017-04-05 17:46:37 +0800

    1060405_H_檔案整理分類及Zip壓縮檔案

--- M10_XmlArrange/Form1.Designer.cs
+++ M10_XmlArrange/Form1.Designer.cs
@@ -28,12 +28,146 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.btnStart = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.lblProcCount = new System.Windows.Forms.Label();
+      this.lblProc = new System.Windows.Forms.Label();
+      this.btnTest = new System.Windows.Forms.Button();
+      this.progressBar1 = new System.Windows.Forms.ProgressBar();
+      this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+      this.btnBack = new System.Windows.Forms.Button();
+      this.lblProgress = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // btnStart
+      // 
+      this.btnStart.Location = new System.Drawing.Point(361, 284);
+      this.btnStart.Name = "btnStart";
+      this.btnStart.Size = new System.Drawing.Size(75, 23);
+      this.btnStart.TabIndex = 0;
+      this.btnStart.Text = "啟動";
+      this.btnStart.UseVisualStyleBackColor = true;
+      this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+      this.label1.Location = new System.Drawing.Point(12, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(105, 24);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "執行進度：";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+      this.label2.Location = new System.Drawing.Point(12, 47);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(105, 24);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "執行程序：";
+      // 
+      // lblProcCount
+      // 
+      this.lblProcCount.AutoSize = true;
+      this.lblProcCount.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+      this.lblProcCount.Location = new System.Drawing.Point(123, 9);
+      this.lblProcCount.Name = "lblProcCount";
+      this.lblProcCount.Size = new System.Drawing.Size(48, 24);
+      this.lblProcCount.TabIndex = 3;
+      this.lblProcCount.Text = "進度";
+      // 
+      // lblProc
+      // 
+      this.lblProc.AutoSize = true;
+      this.lblProc.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+      this.lblProc.Location = new System.Drawing.Point(123, 47);
+      this.lblProc.Name = "lblProc";
+      this.lblProc.Size = new System.Drawing.Size(48, 24);
+      this.lblProc.TabIndex = 4;
+      this.lblProc.Text = "程序";
+      // 
+      // btnTest
+      // 
+      this.btnTest.Location = new System.Drawing.Point(442, 284);
+      this.btnTest.Name = "btnTest";
+      this.btnTest.Size = new System.Drawing.Size(75, 23);
+      this.btnTest.TabIndex = 5;
+      this.btnTest.Text = "測試";
+      this.btnTest.UseVisualStyleBackColor = true;
+      this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+      // 
+      // progressBar1
+      // 
+      this.progressBar1.Location = new System.Drawing.Point(12, 205);
+      this.progressBar1.Name = "progressBar1";
+      this.progressBar1.Size = new System.Drawing.Size(794, 23);
+      this.progressBar1.TabIndex = 6;
+      // 
+      // backgroundWorker1
+      // 
+      this.backgroundWorker1.WorkerReportsProgress = true;
+      this.backgroundWorker1.WorkerSupportsCancellation = true;
+      this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+      this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+      // 
+      // btnBack
+      // 
+      this.btnBack.Location = new System.Drawing.Point(523, 284);
+      this.btnBack.Name = "btnBack";
+      this.btnBack.Size = new System.Drawing.Size(75, 23);
+      this.btnBack.TabIndex = 7;
+      this.btnBack.Text = "背景";
+      this.btnBack.UseVisualStyleBackColor = true;
+      this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+      // 
+      // lblProgress
+      // 
+      this.lblProgress.AutoSize = true;
+      this.lblProgress.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+      this.lblProgress.Location = new System.Drawing.Point(12, 178);
+      this.lblProgress.Name = "lblProgress";
+      this.lblProgress.Size = new System.Drawing.Size(48, 24);
+      this.lblProgress.TabIndex = 8;
+      this.lblProgress.Text = "程序";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(818, 319);
+      this.Controls.Add(this.lblProgress);
+      this.Controls.Add(this.btnBack);
+      this.Controls.Add(this.progressBar1);
+      this.Controls.Add(this.btnTest);
+      this.Controls.Add(this.lblProc);
+      this.Controls.Add(this.lblProcCount);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.btnStart);
+      this.Name = "Form1";
       this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
     }
 
     #endregion
+
+    private System.Windows.Forms.Button btnStart;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label lblProcCount;
+    private System.Windows.Forms.Label lblProc;
+    private System.Windows.Forms.Button btnTest;
+    private System.Windows.Forms.ProgressBar progressBar1;
+    private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    private System.Windows.Forms.Button btnBack;
+    private System.Windows.Forms.Label lblProgress;
   }
 }
 
--- M10_XmlArrange/Form1.cs
+++ M10_XmlArrange/Form1.cs
@@ -7,14 +7,138 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Ionic.Zip;
 
 namespace M10_XmlArrange
 {
   public partial class Form1 : Form
   {
+    const string sBakFilePath = @"C:\m10\bak";
+    const string sFilePathArrange = @"C:\m10\Arrange";
+    const string sFilePathProblem = @"C:\m10\Problem";
+    string sZipTarDirectory = @"c:\m10\Arrange";
+    string sZipDesDirectory = @"c:\m10\ArrangeZip"; 
     public Form1()
     {
       InitializeComponent();
+
+
+      
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      lblProcCount.Text = "";
+      lblProc.Text = "";
+
+      if (!Directory.Exists(sBakFilePath)) Directory.CreateDirectory(sBakFilePath);
+      if (!Directory.Exists(sFilePathArrange)) Directory.CreateDirectory(sFilePathArrange);
+    }
+
+    private void btnStart_Click(object sender, EventArgs e)
+    {
+      List<string> ListFile = Directory.GetFiles(sBakFilePath).ToList<string>();
+
+      int iIndex = 0;
+      //取得所有檔案
+      foreach (string sFile in ListFile)
+      {
+        iIndex++;
+
+        lblProcCount.Text = string.Format("{0}/{1}", iIndex.ToString(), ListFile.Count.ToString());
+        lblProcCount.Refresh();
+
+        string sSaveFolder = "";
+        FileInfo fi = new FileInfo(sFile);
+
+        //解析檔案年月日
+        string sYear = "";
+        string sMonth = "";
+        string sDay = "";
+
+        List<string> slist = fi.Name.Split('_').ToList<string>();
+
+        if (slist.Count < 4) continue;
+
+        //分類
+        sSaveFolder = string.Format(@"{0}\{1}\{2}\{3}\", sFilePathArrange, slist[0], slist[1], slist[2]);
+        if (!Directory.Exists(sSaveFolder)) Directory.CreateDirectory(sSaveFolder);
+
+        //歸檔
+        //檔案移至整理路徑
+        lblProc.Text = string.Format("{0}：移動至{1}", fi.Name, sSaveFolder + fi.Name);
+        lblProc.Refresh();
+        File.Move(fi.FullName, sSaveFolder + fi.Name);
+
+      }
+
+      //壓縮
+      //using (ZipFile zip = new ZipFile(@"c:\test.zip"))
+      //{
+      //  zip.AddFile("");
+
+
+      //  zip.Save();
+
+      //}
+
+
+
+    }
+
+    private void btnTest_Click(object sender, EventArgs e)
+    {
+      //壓縮
+      using (ZipFile zip = new ZipFile(Encoding.Default))
+      { 
+        zip.SaveProgress += zipProgress;
+        //大檔壓縮
+        zip.UseZip64WhenSaving = Zip64Option.AsNecessary;       
+
+        //檔案名稱
+        string sZipName = DateTime.Now.ToString("yyyy_MM_dd_HH_mm");
+        //忽略重複檔案
+        zip.IgnoreDuplicateFiles = true;
+        zip.AddDirectory(sZipTarDirectory,"123");
+        zip.Save(string.Format(@"{0}\{1}.zip",sZipDesDirectory, sZipName));
+      }
+    }
+
+    private void zipProgress(object sender, SaveProgressEventArgs e)
+    {
+      if (e.EventType == ZipProgressEventType.Saving_AfterWriteEntry)
+      {
+        backgroundWorker1.ReportProgress(e.EntriesSaved * 100 / e.EntriesTotal);
+        
+        //this.progressBar1.Value = e.EntriesSaved * 100 / e.EntriesTotal;
+        
+      }
+      //if (e.EventType == ZipProgressEventType.Saving_EntryBytesRead)
+      //  this.progressBar1.Value = (int)((e.BytesTransferred * 100) / e.TotalBytesToTransfer);
+
+      else if (e.EventType == ZipProgressEventType.Saving_Completed)
+      {
+        backgroundWorker1.ReportProgress(100);
+        //this.progressBar1.Value = 100;
+      }
+        
+    }
+
+    private void btnBack_Click(object sender, EventArgs e)
+    {
+      backgroundWorker1.RunWorkerAsync();
+    }
+
+    private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+    {
+      btnTest_Click(sender, e);
+    }
+
+    private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+    {
+      progressBar1.Value = e.ProgressPercentage;
+      lblProgress.Text = string.Format("壓縮執行進度：{0}%", e.ProgressPercentage.ToString());
     }
   }
 }
