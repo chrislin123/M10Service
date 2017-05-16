2c29e23d4ccdfc141e199a6222ba8380bd9e0531
chrislin <chris.lin.tw@hotmail.com>
2017-05-16 17:40:21 +0800

    1060516_C_新增警戒歷史紀錄查詢

--- M10Api/Controllers/WarnController.cs
+++ M10Api/Controllers/WarnController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using M10Api.Class;
 using Dapper;
 using Dapper.Contrib.Extensions;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+//using System.Web.Http;
 
 
 namespace M10Api.Controllers
@@ -59,6 +62,107 @@ namespace M10Api.Controllers
           item.ELRTI = Math.Floor(dELRTI).ToString();
         } 
         
+      }
+
+
+      ViewBag.count = data.Count;
+      ViewData["LRTIAlert"] = data;
+
+      return View();
+    }
+
+    public ActionResult warnhislist()
+    {
+      //var AlertUpdateTm = db.Query(@" select * from LRTIAlertMail where type = 'altm' ");
+
+
+
+      //ViewBag.nowdate = "";
+      //if (AlertUpdateTm.Count > 0)
+      //{
+      //  ViewBag.nowdate = AlertUpdateTm[0].value;
+      //}
+
+      string ssql = @" select * from LRTIAlert where status = '{0}' order by country,town ";
+      //新增
+      var dataI = db.Query(string.Format(ssql, "I"));
+      //持續
+      var dataC = db.Query(string.Format(ssql, "C"));
+      //解除
+      var dataD = db.Query(string.Format(ssql, "D"));
+
+
+      List<dynamic> data = new List<dynamic>();
+      data.AddRange(dataI);
+      data.AddRange(dataC);
+      data.AddRange(dataD);
+
+      foreach (var item in data)
+      {
+        //處理狀態改中文顯示
+        if (item.status == "I") item.status = "新增";
+        if (item.status == "C") item.status = "持續";
+        if (item.status == "D") item.status = "刪除";
+
+
+        //處理ELRTI無條件捨去
+        decimal dELRTI = 0;
+        if (decimal.TryParse(Convert.ToString(item.ELRTI), out dELRTI))
+        {
+          item.ELRTI = Math.Floor(dELRTI).ToString();
+        }
+
+      }
+
+
+      ViewBag.count = data.Count;
+      ViewData["LRTIAlert"] = data;
+
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult warnhislistq(JObject json)
+    {
+      //var AlertUpdateTm = db.Query(@" select * from LRTIAlertMail where type = 'altm' ");
+
+
+
+      //ViewBag.nowdate = "";
+      //if (AlertUpdateTm.Count > 0)
+      //{
+      //  ViewBag.nowdate = AlertUpdateTm[0].value;
+      //}
+
+      string ssql = @" select * from LRTIAlert where status = '{0}' order by country,town ";
+      //新增
+      var dataI = db.Query(string.Format(ssql, "I"));
+      //持續
+      var dataC = db.Query(string.Format(ssql, "C"));
+      //解除
+      var dataD = db.Query(string.Format(ssql, "D"));
+
+
+      List<dynamic> data = new List<dynamic>();
+      data.AddRange(dataI);
+      data.AddRange(dataC);
+      data.AddRange(dataD);
+
+      foreach (var item in data)
+      {
+        //處理狀態改中文顯示
+        if (item.status == "I") item.status = "新增";
+        if (item.status == "C") item.status = "持續";
+        if (item.status == "D") item.status = "刪除";
+
+
+        //處理ELRTI無條件捨去
+        decimal dELRTI = 0;
+        if (decimal.TryParse(Convert.ToString(item.ELRTI), out dELRTI))
+        {
+          item.ELRTI = Math.Floor(dELRTI).ToString();
+        }
+
       }
 
 
