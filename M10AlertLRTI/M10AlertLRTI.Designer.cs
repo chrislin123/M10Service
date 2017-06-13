0fa43adf2b79758810c3e3b0557180f4004e7e9b
chrislin <chris.lin.tw@hotmail.com>
2017-06-13 17:23:35 +0800

    1060613_c_加上HighRail轉檔

--- M10AlertLRTI/M10AlertLRTI.Designer.cs
+++ M10AlertLRTI/M10AlertLRTI.Designer.cs
@@ -28,37 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.btnStart = new System.Windows.Forms.Button();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.SuspendLayout();
-            // 
-            // btnStart
-            // 
-            this.btnStart.Location = new System.Drawing.Point(24, 32);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
-            this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "開始";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
-            // 
-            // timer1
-            // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 1000;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // M10AlertLRTI
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.btnStart);
-            this.Name = "M10AlertLRTI";
-            this.Text = "警戒值警示功能";
-            this.Load += new System.EventHandler(this.M10AlertLRTI_Load);
-            this.ResumeLayout(false);
+      this.components = new System.ComponentModel.Container();
+      this.btnStart = new System.Windows.Forms.Button();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.button1 = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // btnStart
+      // 
+      this.btnStart.Location = new System.Drawing.Point(24, 32);
+      this.btnStart.Name = "btnStart";
+      this.btnStart.Size = new System.Drawing.Size(75, 23);
+      this.btnStart.TabIndex = 0;
+      this.btnStart.Text = "開始";
+      this.btnStart.UseVisualStyleBackColor = true;
+      this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+      // 
+      // timer1
+      // 
+      this.timer1.Interval = 1000;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(105, 120);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "開始";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // M10AlertLRTI
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(284, 262);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.btnStart);
+      this.Name = "M10AlertLRTI";
+      this.Text = "警戒值警示功能";
+      this.Load += new System.EventHandler(this.M10AlertLRTI_Load);
+      this.ResumeLayout(false);
 
         }
 
@@ -66,6 +77,7 @@
 
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Timer timer1;
-    }
+    private System.Windows.Forms.Button button1;
+  }
 }
 
--- M10AlertLRTI/M10AlertLRTI.cs
+++ M10AlertLRTI/M10AlertLRTI.cs
@@ -664,6 +664,225 @@ namespace M10AlertLRTI
 
       this.Close();
     }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      DataTable dtresult = new DataTable();
+      dtresult.Columns.Add("country");
+      dtresult.Columns.Add("week");
+      dtresult.Columns.Add("car");
+      dtresult.Columns.Add("time");
+
+      string sFilepath = @"c:\test.csv";
+
+      //DataTable dt = CSVtoDataTable(sFilepath);
+
+
+
+      List<string> ll = new List<string>();
+
+      using (StreamReader SR = new StreamReader(sFilepath))
+      {
+        string Line;
+        while ((Line = SR.ReadLine()) != null)
+        {
+          if (Line == "") continue;
+          if (Line == "\"             車次") continue;
+          if (Line == ",,,,,,,,,,,,") continue;
+          if (Line == "北上(Northbound),,,,,,,,,,,,") continue;
+          if (Line == "台   灣   高   鐵   列   車   時   刻   表,,,,,,,,,,,,") continue;
+          if (Line == ",=,=,=,=,=,=,=,=,=,=,,") continue;
+          if (Line == "註 : 本時刻表所列各次列車，於起站與中間站均為開車時刻，於終點站為到達時刻。,,,,,,,,,,,,") continue;
+          string[] ReadLine_Array = Line.Split(',');
+          if (ReadLine_Array[0] == "" && ReadLine_Array[1] == "" && ReadLine_Array[2] == "")
+          {
+            continue;
+          }
+          if (ReadLine_Array[0] == "站名\"")
+          {
+            continue;
+          }
+          if (Line == ",=,,,,,,,,,,,") continue;
+          if (Line == ",,=,,,,,,,,,,") continue;
+
+
+
+          ll.Add(Line);
+          //MessageBox.Show(Line);
+
+          //string[] ReadLine_Array = Line.Split(',');
+          //這邊可以自行發揮
+        }
+      }
+
+
+      DataTable dt = new DataTable();
+
+      for (int i = 0; i < ll.Count; i++)
+      {
+        string[] ReadLine_Array = ll[i].Split(',');
+
+        if (i == 0)
+        {
+          foreach (string item in ReadLine_Array)
+          {
+            dt.Columns.Add();
+          }
+        }
+
+
+        DataRow newRow = dt.NewRow();
+        for (int j = 0; j < dt.Columns.Count; j++)
+        {
+          newRow[j] = ReadLine_Array[j];
+        }
+        dt.Rows.Add(newRow);
+      }
+      dt.Columns.RemoveAt(12);
+      dt.Columns.RemoveAt(11);
+
+
+
+      List<string> runday = new List<string>();
+      List<string> car = new List<string>();
+
+      foreach (DataRow LoopRow in dt.Rows)
+      {
+        if (LoopRow[0].ToString() == "行駛日")
+        {
+          runday.Clear();
+          for (int i = 0; i < dt.Columns.Count; i++)
+          {
+            runday.Add(LoopRow[i].ToString());
+          }
+        }
+        if (LoopRow[0].ToString() == "")
+        {
+          car.Clear();
+          for (int i = 0; i < dt.Columns.Count; i++)
+          {
+            car.Add(LoopRow[i].ToString());
+          }
+        }
+
+        if (LoopRow[0].ToString() != "行駛日" && LoopRow[0].ToString() != "")
+        {
+          string sCountry = LoopRow[0].ToString();
+          for (int i = 1; i < dt.Columns.Count; i++)
+          {
+            string scar = car[i];
+            string srunday = runday[i];
+            if (LoopRow[i].ToString() == "" || LoopRow[i].ToString() == "↓") continue;
+            string stime = LoopRow[i].ToString();
+
+            //解析星期
+            List<string> lweek = analyweek(srunday);
+
+            foreach (string Loopweek in lweek)
+            {
+              DataRow NewRow = dtresult.NewRow();
+              NewRow["country"] = sCountry;
+              NewRow["week"] = Loopweek;
+              NewRow["car"] = scar;
+              NewRow["time"] = stime;
+              dtresult.Rows.Add(NewRow);
+            }
+          }
+        }
+
+
+
+      }
+
+
+
+      foreach (DataRow insRow in dtresult.Rows)
+      {
+        ssql = @"INSERT INTO [dbo].[highrail]
+           ([country]
+           ,[week]
+           ,[car]
+           ,[time])
+            VALUES
+           ('{0}'
+           ,'{1}'
+           ,'{2}'
+           ,'{3}')";
+        oDal.CommandText = string.Format(ssql, insRow[0].ToString()
+          , insRow[1].ToString(), insRow[2].ToString(), insRow[3].ToString());
+        oDal.ExecuteSql();
+      }
+
+
+
+      MessageBox.Show("Test");
+
+    }
+
+    private List<string> analyweek(string sweek)
+    {
+      List<string> lResult = new List<string>();
+
+      if (sweek == "")
+      {
+        lResult.Add("1");
+        lResult.Add("2");
+        lResult.Add("3");
+        lResult.Add("4");
+        lResult.Add("5");
+        lResult.Add("6");
+        lResult.Add("7");
+      }
+      else
+      {
+        string[] aweek1 = sweek.Split('、');
+        foreach (string item in aweek1)
+        {
+          if (item == "一") lResult.Add("1");
+          if (item == "二") lResult.Add("2");
+          if (item == "三") lResult.Add("3");
+          if (item == "四") lResult.Add("4");
+          if (item == "五") lResult.Add("5");
+          if (item == "六") lResult.Add("6");
+          if (item == "日") lResult.Add("7");
+
+          if (item.Contains("~"))
+          {
+            string[] aweek2 = item.Split('~');
+
+            int iStart = parseweek(aweek2[0]);
+            int iEnd = parseweek(aweek2[1]);
+
+            for (int i = iStart; i <= iEnd; i++)
+            {
+              lResult.Add(i.ToString());
+            }
+          }
+        }
+      }
+
+
+
+
+      return lResult;
+    }
+
+    private int parseweek(string sw)
+    {
+      int ir = 0;
+
+      if (sw == "一") ir = 1;
+      if (sw == "二") ir = 2;
+      if (sw == "三") ir = 3;
+      if (sw == "四") ir = 4;
+      if (sw == "五") ir = 5;
+      if (sw == "六") ir = 6;
+      if (sw == "日") ir = 7;
+
+      return ir;
+    }
+
+
   }
 
 }
