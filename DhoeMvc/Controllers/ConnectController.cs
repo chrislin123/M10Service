a472049f28118e0e944b4b0edce386511424e0b4
chrislin <chris.lin.tw@hotmail.com>
2017-07-28 15:42:03 +0800

    1060728_C_Dhoe改版

--- DhoeMvc/Controllers/ConnectController.cs
+++ DhoeMvc/Controllers/ConnectController.cs
@@ -7,92 +7,35 @@ using DhoeMvc.Class;
 
 namespace DhoeMvc.Controllers
 {
-    public class ConnectController : BaseController
-    {
+  public class ConnectController : BaseController
+  {
 
     public ActionResult Connect()
     {
-     
+
+      List<dynamic> DataList = dbDapper.Query(@"select * from Connect");
+
+      //List<Newsm> NewsmList = dbDapper.Query<Newsm>(@"select * from newsm
+      //                  order by date desc");
+
+      int iRowIndex = 0;
+      foreach (var item in DataList)
+      {
+        iRowIndex++;
+
+        item.type = "";
+        if (iRowIndex % 2 == 1)
+        {
+          item.type = "1";
+        }
+      }
+
+
+      ViewData["DataList"] = DataList;
+
       return View();
     }
 
 
-    // GET: Connect
-    public ActionResult Index()
-        {
-            return View();
-        }
-
-        // GET: Connect/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Connect/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Connect/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Connect/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Connect/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Connect/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Connect/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }
+  }
 }
---
+++ M10.lib/model/modeldhoe.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+
+namespace M10.lib.modeldhoe
+{
+  class modeldhoe
+  {
+  }
+
+  [Table("Newsm")]
+  public class Newsm
+  {
+    //設定key
+    [Key]
+    public int no { get; set; }
+
+    public string date { get; set; }
+
+    public string title { get; set; }
+
+    public string link { get; set; }
+
+    public string emp { get; set; }
+
+    public string dttm { get; set; }
+
+  }
+
+  [Table("Connect")]
+  public class Connect
+  {
+    //設定key
+    [Key]
+    public int no { get; set; }
+
+    public string date { get; set; }
+
+    public string title { get; set; }
+
+    public string link { get; set; }
+
+    public string emp { get; set; }
+
+    public string dttm { get; set; }
+
+  }
+
+  [Table("AlbumM")]
+  public class AlbumM
+  {
+    //設定key
+    [Key]
+    public int intseq { get; set; }
+
+    public string name { get; set; }
+
+    public int? sort { get; set; }
+
+    public string emp { get; set; }
+
+    public string dttm { get; set; }
+
+  }
+
+  [Table("Books")]
+  public class Books
+  {
+    //設定key
+    [Key]
+    public int inseq { get; set; }
+
+    public string date { get; set; }
+
+    public string content { get; set; }
+
+    public string emp { get; set; }
+
+    public string dttm { get; set; }
+
+  }
+
+  [Table("CenterMember")]
+  public class CenterMember
+  {
+    //設定key
+    [Key]
+    public int inseq { get; set; }
+
+    public string name { get; set; }
+
+    public string content { get; set; }
+
+    public string emp { get; set; }
+
+    public string dttm { get; set; }
+
+  }
+
+  [Table("CenterProject")]
+  public class CenterProject
+  {
+    //設定key
+    [Key]
+    public int inseq { get; set; }
+
+    public string name { get; set; }
+
+    public string content { get; set; }
+
+    public string emp { get; set; }
+
+    public string dttm { get; set; }
+
+  }
+
+  [Table("ConfPaper")]
+  public class ConfPaper
+  {
+    //設定key
+    [Key]
+    public int inseq { get; set; }
+
+    public string DATE { get; set; }
+
+    public string content { get; set; }
+
+    public string emp { get; set; }
+
+    public string dttm { get; set; }
+
+  }
+
+  [Table("JournalPaper")]
+  public class JournalPaper
+  {
+    //設定key
+    [Key]
+    public int intseq { get; set; }
+
+    public string date { get; set; }
+
+    public string content { get; set; }
+
+    public string emp { get; set; }
+
+    public string dttm { get; set; }
+
+  }
+
+  [Table("Publications")]
+  public class Publications
+  {
+    //設定key
+    [Key]
+    public int inseq { get; set; }
+
+    public string DATE { get; set; }
+
+    public string content { get; set; }
+
+    public string emp { get; set; }
+
+    public string dttm { get; set; }
+
+  }
+
+  [Table("Students")]
+  public class Students
+  {
+    //設定key
+    [Key]
+    public int inseq { get; set; }
+
+    public string kind { get; set; }
+
+    public string name { get; set; }
+
+    public string emp { get; set; }
+
+    public string dttm { get; set; }
+
+    public string datatype { get; set; }
+
+  }
+
+
+}
