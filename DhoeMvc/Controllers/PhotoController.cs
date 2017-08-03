f85339182ec60e21d0aa05c33fda9666ac233a38
chrislin <chris.lin.tw@hotmail.com>
2017-08-03 14:00:59 +0800

    1060803_C_調整photo

--- DhoeMvc/Controllers/PhotoController.cs
+++ DhoeMvc/Controllers/PhotoController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DhoeMvc.Class;
+using M10.lib.modeldhoe;
 
 namespace DhoeMvc.Controllers
 {
-    public class PhotoController : Controller
-    {
-        // GET: Photo
-        public ActionResult Photo()
-        {
-            return View();
-        }
+  public class PhotoController : BaseController
+  {
+    // GET: Photo
+    public ActionResult Photo()
+    { 
+      List<AlbumM> DataList = dbDapper.Query<AlbumM>(@"select * from AlbumM
+                        order by sort desc");
+
+      ViewData["DataList"] = DataList;
+
+      return View();
     }
+  }
 }
