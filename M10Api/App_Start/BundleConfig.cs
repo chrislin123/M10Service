130f1c07e7415fa294d0be1979dd54098905c356
chrislin123 <chris.lin.tw@hotmail.com>
2018-09-12 17:35:28 +0800

    1070912_C_QueryRain新增排序功能

--- M10Api/App_Start/BundleConfig.cs
+++ M10Api/App_Start/BundleConfig.cs
@@ -17,12 +17,16 @@ namespace M10Api
                   "~/Scripts/modernizr-*"));
 
       bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
+                  "~/Scripts/bootstrap.js"
+                , "~/Scripts/respond.js"
+                , "~/Scripts/bootstrap-table.min.js"
+                ));
 
       bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"));
+                  "~/Content/bootstrap.css"
+                , "~/Content/site.css"
+                , "~/Content/bootstrap-table.min.css"
+                ));
     }
   }
 }
